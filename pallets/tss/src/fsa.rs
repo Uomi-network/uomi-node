3571cd16e149b705086eb0db14f8e60975ae3f45
Luca Simonetti <lucasimonetti21@gmail.com>
2025-08-27 17:03:18 +0200

    fix: update rpc_gas_limit to be an Option in build_or_passthrough function

--- pallets/tss/src/fsa.rs
+++ pallets/tss/src/fsa.rs
@@ -289,7 +289,7 @@ fn build_or_passthrough(action: &Action) -> Result<Option<(u32, Vec<u8>)>, Proce
             // if let Some(ref from_addr) = action.from {
             //     rpc_gas_limit = try_estimate_gas(action.chain_id, from_addr, to, action.value.as_ref().and_then(|v| parse_num_u64("value", v)), &data_bytes);
             // }
-            rpc_gas_limit = 25_000; // todo: fix.
+            rpc_gas_limit = Some(25_000); // todo: fix.
         }
         let gas_limit = action.gas_limit.as_ref().and_then(|v| parse_num_u64("gas_limit", v)).or(rpc_gas_limit).unwrap_or(21_000);
 
