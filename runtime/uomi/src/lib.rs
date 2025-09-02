0b81a2268cf676a6a19158d17232dc89a331dbe1
Luca Simonetti <lucasimonetti21@gmail.com>
2025-09-02 09:45:34 +0200

    fix(runtime): update initial balance for year zero and add reminder for mainnet removal

--- runtime/uomi/src/lib.rs
+++ runtime/uomi/src/lib.rs
@@ -2753,6 +2753,7 @@ impl EraPayout<Balance> for UOMIEraPayout {
         let halving_period = 3; // Years before halving
         
         let year_zero: Balance = 4919219238u128.into();
+        let year_zero: Balance = 400000000000u128.into(); // TODO: SUPER MEGA EXTRA UBER IMPORTANT REMEMBER TO REMOVE IN MAINNET
         let base_issuance_per_three_year: Balance = (2682750000u128 * halving_period).into();
         let factor = 2;
 
